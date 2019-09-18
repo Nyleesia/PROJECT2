6c75d308b206aeb305dfc8307e4f95305304b95a
Steven Dixon <stevendixon112@gmail.com>
2019-09-17 20:18:20 -0400

    added login system

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,43 @@
-var db = require("../models");
+const db = require("../models");
+const passport = require("../config/passport");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+const express = require("express");
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+const router = express.Router();
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+router.post("/login", passport.authenticate("local"), function(req, res) {
+  res.json("/members");
+});
+
+
+router.post("/signup", function(req, res) {
+  console.log(req.body);
+  db.User.create({
+    email: req.body.email,
+    password: req.body.password
+  }).then(function() {
+    res.redirect(307, "/api/login");
+  }).catch(function(err) {
+    console.log(err);
+    res.json(err);
   });
-};
+});
+
+//
+// Route for getting some data about our user to be used client side
+router.get("/user_data", function(req, res) {
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+  }
+  else {
+    // Otherwise send back the user's email and id
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id
+    });
+  }
+});
+
+module.exports = router;
