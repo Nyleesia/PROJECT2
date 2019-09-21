7a034b137eefc511b3a32ec10f1a1b2be78b4136
Michal Terranova <michalterranova@mail.com>
2019-09-21 16:30:18 -0400

    front-end

--- public/js/app.js
+++ public/js/app.js
@@ -19,14 +19,35 @@ $(".openProjects").on("click", function () {
 // });
 
 // $.get(url).then(function(data){
-//     var projectList = data.user.project;
-//     for (var i =0; i < projectList.length; i++){
-//         var newProject = $("<div>").attr("class", "projectArea");
+// var projectList = data.user.project;
+// for (var i =0; i < projectList.length; i++){  
+//     var newProject =   '<div class="card" id="newProjectCard" style="width: 60rem; margin-left:18%;">'+
+//                         '<p class="card-text text-center"></p>'+
+//                         '<img id="imageProject" src="" class="card-img-top" alt="">'+
+//                         '<div class="card-body">'+
+//                         '<p class="detailsProject">Details:</p>'+
+//                         '</div>'+
+//                         '<ul class="list-group list-group-flush">'+
+//                         '<li class="list-group-item">Host:</li>'+
+//                         '</ul>'+
+//                         '<div class="card-body">'+
+//                         '<a href="" style="float:left;"></a>'+
+//                         '<div class="hours" style="width:75px; height:75px; float:right;"></div>'+'</div>'+'</div>';
+//     $(".userProjectsPending").prepend(newProject);
 
-//         $(".userProjectsPending").prepend(newProject);
+//     var newAttended =  '<div class="card" id="newAttendedCard" style="width: 60rem; margin-left:18%;">'+
+//                         '<p id="imageProject" class="card-text text-center"></p>'+
+//                         '<img src="" class="card-img-top" alt="">'+
+//                         '<div class="card-body">'+
+//                         '<p class="detailsAttended">Details:</p>'+
+//                         '</div>'+
+//                         '<ul class="list-group list-group-flush">'+
+//                         '<li class="list-group-item">Host:</li>'+
+//                         '</ul>'+
+//                         '<div class="card-body">'+
+//                         '<a href="" style="float:left;">link</a>'+
+//                         '<div class="hours" style="width:75px; height:75px; float:right;"></div>'+'</div>'+'</div>';
 
-//         var newAttended = $("<div>").attr("class", "attendedArea");
-
-//         $(".userProjectsAttended").prepend(newAttended);
-//     }
+//     $(".userProjectsAttended").prepend(newAttended);
+// }
 // });
