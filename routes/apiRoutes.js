93f63f51f07c3274aa5cf61dd655159e1a24d99f
Steven Dixon <stevendixon112@gmail.com>
2019-09-22 14:38:38 -0400

    more multer

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, "uploads");
+    cb(null, "public/uploads");
   },
   filename: function(req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now());
