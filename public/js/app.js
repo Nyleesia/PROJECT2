682cf3b6d6dfe86e055dece7c1a5ad185c0e6ce8
Michal Terranova <michalterranova@mail.com>
2019-09-23 20:03:56 -0400

    adding the public folder again

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
-$(".openAttended").on("click", function() {
+$(".openAttended").on("click", function () {
   $(".userHoursSpace").hide();
   $(".userProjectsAttended").show();
   $(".userProjectsPending").hide();
   console.log("hey");
 });
-$(".openProjects").on("click", function() {
+$(".openProjects").on("click", function () {
   $(".userHoursSpace").hide();
   $(".userProjectsAttended").hide();
   $(".userProjectsPending").show();
 });
+projects = [];
+if (projects.length === 0) {
+  var noProject = $("<div>").attr("class", "noProjectAppended");
+  var words =
+    "<h2>Your volunteer box is currently empty. Take a peek at the latest projects posted:   <a href='/projects'>HERE<a><h2>";
+  noProject.append(words);
+  $(".userProjectsPending").append(noProject);
+} else { }
 // $.get("/profile").then(function(data) {
 //   console.log("Hello");
 //   //console.log(data);
